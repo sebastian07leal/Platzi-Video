0a49649e567ce7a14c741b68775af21e276ddf30
Fabian Toro <helber.toro@gmail.com>
2019-10-09 23:14:03 -0500

    Create Sass vars and media queries

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -11,11 +11,24 @@ const App = () => (
     <div className="App">
         <Header />
         <Search />
-        <Categories>
+        <Categories title="Mi lista">
             <Carousel>
                 <CarouselItem />
                 <CarouselItem />
                 <CarouselItem />
+                <CarouselItem />
+            </Carousel>
+        </Categories>
+
+        <Categories title="Tendencias">
+            <Carousel>
+                <CarouselItem />
+                <CarouselItem />
+            </Carousel>
+        </Categories>
+
+        <Categories title="Originales de Platzi Video">
+            <Carousel>
                 <CarouselItem />
             </Carousel>
         </Categories>
