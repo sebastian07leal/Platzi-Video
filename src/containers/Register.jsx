b2d90fea84adef9e922ada4bba347d21faa52643
Fabian Toro <helber.toro@gmail.com>
2019-10-16 22:37:45 -0500

    Register view with styles and images

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Register = () => (
     <section className="container-fullheight">
         <section className="login__container">
-            <h2 className="login__container--titulo">Registrate</h2>
+            <h2 className="login__container--titulo">Regístrate</h2>
             <form className="login__container--form">
                 <input className="input" type="text" placeholder="Nombre" />
                 <input className="input" type="email" placeholder="Correo" />
