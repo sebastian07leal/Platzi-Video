c5c19a66b08b46c7d5d1a5fa0f91015f78c29b99
Fabian Toro <helber.toro@gmail.com>
2019-10-19 22:21:58 -0500

    using gravatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import gravatar from '../utils/gravatar';
 import '../assets/styles/components/Header.sass';
 import logo from '../assets/static/platzi-video-logo.png';
 import userIcon from '../assets/static/user-icon.png';
 
-const Header = () => (
-    <header className="header">
-        <Link to="/">
-            <img className="header__img" src={logo} alt="Platzi Video"/>
-        </Link>
-        <div className="header__menu">
-            <div className="header__menu--profile">
-                <img src={userIcon} alt="Icono Usuario"/>
-                <p>Perfil</p>
+const Header = () => {
+    return (
+        <header className="header">
+            <Link to="/">
+                <img className="header__img" src={logo} alt="Platzi Video"/>
+            </Link>
+            <div className="header__menu">
+                <div className="header__menu--profile">
+                    <img src={gravatar('helber.toro@gmail.com')} alt="Icono Usuario"/>
+                    <p>Perfil</p>
+                </div>
+                <ul>
+                    <li><a href="/">Cuenta</a></li>
+                    <li>
+                        <Link to="/login">
+                            Iniciar Sesión
+                        </Link>
+                    </li>
+                </ul>
             </div>
-            <ul>
-                <li><a href="/">Cuenta</a></li>
-                <li>
-                    <Link to="/login">
-                        Iniciar Sesión
-                    </Link>
-                </li>
-            </ul>
-        </div>
-    </header>
-)
+        </header>
+    )
+};
 
 export default Header;
