e634fe5b9d906d95633d635cca8cca2614f7d613
Fabian Toro <helber.toro@gmail.com>
2019-10-19 21:49:02 -0500

    Add and delete myList items

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -13,7 +13,11 @@ const Home = ({myList, trends, originals}) => (
             <Categories title="Mi lista">
                 <Carousel>
                     {myList.map(item => 
-                        <CarouselItem key={item.id} {...item} />
+                        <CarouselItem 
+                            key={item.id}
+                            {...item}
+                            isList
+                        />
                     )}
                 </Carousel>
             </Categories>
